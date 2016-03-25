85532131c932317e470478afd5b14d3a69c30581
todeschi <adrien.todeschini@inria.fr>
2016-03-25 01:37:48 +0100

    get

--- DVR/unique_ips.php
+++ DVR/unique_ips.php
@@ -2,5 +2,5 @@
 require(realpath(dirname(__FILE__))."/config.php");
 require(realpath(dirname(__FILE__))."/DeviceTable.php");
 $table = new DVR\DeviceTable(DVR\CONFIG_PATH);
-print_r($table->uniqueIps());
+print_r($table->getUniqueIps());
 ?>
