d92dea2ee5d2505fe6ea66dc3e76bcc8494445a0
todeschi <adrien.todeschini@inria.fr>
2016-03-27 19:25:25 +0200

    define

--- DVR/config.php
+++ DVR/config.php
@@ -3,8 +3,8 @@
 namespace DVR;
 
 const VERSION = '0.1';
-
 const DEBUG = false;
+
 if (DEBUG) {
 	// display errors
 	ini_set('display_errors', 1);
@@ -16,16 +16,16 @@ if (DEBUG) {
 // "../.dvr"
 define('DVR\ROOT_DIR', dirname(__DIR__) . '/.dvr');
 /** @var string default absolute path to config file */
-const CONFIG_PATH = ROOT_DIR . '/dvr.conf';
+define('DVR\CONFIG_PATH', ROOT_DIR . '/dvr.conf');
 /** @var string default absolute path to config file */
-const CONFIG_SYS_PATH = ROOT_DIR . '/dvr.sys.conf';
+define('DVR\CONFIG_SYS_PATH', ROOT_DIR . '/dvr.sys.conf');
 /** @var string default absolute path to passwords file. use '' to disable */
-const PASSWD_PATH = ROOT_DIR . '/dvr.passwd';
+define('DVR\PASSWD_PATH', ROOT_DIR . '/dvr.passwd');
 /** @var string default absolute path to log file */
-const LOG_PATH = ROOT_DIR . '/dvr.log';
+define('DVR\LOG_PATH', ROOT_DIR . '/dvr.log');
 /** @var int default max number of devices per user in the table */
-const MAX_DEVICES = 20;
+define('DVR\MAX_DEVICES', 20);
 /** @var int default file and directory creation mode (0 prefix needed) */
-const CREATE_MODE = 0774;
+define('DVR\CREATE_MODE', 0774);
 
 ?>
